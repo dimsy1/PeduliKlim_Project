74259c3dcfa0de44224bcf6a4491e362f0c85d6b
dimsy1 <dimasy9@gmail.com>
2024-12-17 10:49:36 +0700

    update/fiximage/Logo

--- src/components/Logo-PeduliKlim.jsx
+++ src/components/Logo-PeduliKlim.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="flex gap-2 items-center">
       <Link to='/' className="flex gap-2 items-center">
-        <img src="src/assets/PeduliKlimLogo.png" alt="PeduliKlim Logo" className="h-8" />
+        <img src="assets/PeduliKlimLogo.png" alt="PeduliKlim Logo" className="h-8" />
         <span className="text-green-600 text-xl font-bold">
           <span className="text-gray-900">Peduli</span>Klim
         </span>
